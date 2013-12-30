80b058d0362578bb630260be693b158a28cf1f99
Filipa Andrade <filipa.andrade@gmail.com>
2013-12-30 11:44:12 +0000

    Improved import command

--- base/management/commands/import_ammo.py
+++ base/management/commands/import_ammo.py
@@ -3,57 +3,115 @@ import os
 
 from django.core.management import BaseCommand
 
+from base.models import Ammo, AmmoCaliber, AmmoCover, AmmoGunpowder, \
+    AmmoProjectile, AmmoTip
+
+
+def fix_float(value):
+    if not value:
+        return None
+
+    if ',' in value:
+        value = value.replace(',', '.')
+
+    for end in ('g', 'mm'):
+        if value.endswith(end):
+            value = value.replace(end, '')
+
+    return float(value)
+
 
 class Command(BaseCommand):
     """Reads the csv file and puts it in the db"""
 
     def get_values(self, line):
         result = dict()
-        result['caliber'] = line[0]
-        result['head_stamp'] = line[1]
-        result['year'] = line[2]
-        result['ammo_type'] = line[3]
-        result['varnish_color'] = line[4]
-        result['tip_color'] = line[5]
-        result['tip_type'] = line[6]
-        result['tip_shape'] = line[7]
-        result['projectile_diameter'] = line[8]
-        result['cover_length'] = line[9]
-        result['country'] = line[10]
-        result['factory'] = line[11]
-        result['caliber_1'] = line[12]
-        result['caliber_2'] = line[13]
-        result['caliber_3'] = line[14]
-        result['caliber_4'] = line[15]
-        result['total_weight'] = line[16]
-        result['weight'] = line[17]
-        result['has_magnetic_properties'] = line[18]
-        result['projectile_material'] = line[19]
-        result['sulco_serrilhado'] = line[20]
-        result['projectile_weight'] = line[21]
-        result['cover_material'] = line[22]
-        result['cover_type'] = line[23]
-        result['cover_weight'] = line[24]
-        result['gunpowder_type'] = line[25]
-        result['gunpowder_color'] = line[26]
-        result['percussion_type'] = line[27]
-        result['notes'] = line[28]
+
+        # Ammo
+        kwargs_ammo = dict()
+        kwargs_ammo['caliber'] = line[0].strip()
+        kwargs_ammo['head_stamp'] = line[1].strip()
+        kwargs_ammo['year'] = line[2].strip()
+        kwargs_ammo['ammo_type'] = line[3].strip()
+        kwargs_ammo['varnish_color'] = line[4].strip()
+        kwargs_ammo['country'] = line[10].strip()
+        kwargs_ammo['factory'] = line[11].strip()
+        kwargs_ammo['total_weight'] = fix_float(line[16].strip())
+        kwargs_ammo['weight'] = fix_float(line[17].strip())
+        kwargs_ammo['percussion_type'] = line[27].strip()
+        kwargs_ammo['notes'] = line[28].strip()
+
+        # Caliber
+        kwargs_calibers = dict()
+        kwargs_calibers['caliber_1'] = line[12].strip()
+        kwargs_calibers['caliber_2'] = line[13].strip()
+        kwargs_calibers['caliber_3'] = line[14].strip()
+        kwargs_calibers['caliber_4'] = line[15].strip()
+
+        # Tip
+        kwargs_tip = dict()
+        kwargs_tip['tip_color'] = line[5].strip()
+        kwargs_tip['tip_type'] = line[6].strip()
+        kwargs_tip['tip_shape'] = line[7].strip()
+
+        # Projectile
+        kwargs_projectile = dict()
+        kwargs_projectile['projectile_diameter'] = fix_float(line[8].strip())
+        kwargs_projectile['projectile_material'] = line[19].strip()
+        kwargs_projectile['projectile_weight'] = fix_float(line[21].strip())
+        kwargs_projectile['has_magnetic_properties'] = line[18].strip() == 'Sim'
+        kwargs_projectile['sulco_serrilhado'] = line[20].strip()
+
+        # Cover
+        kwargs_cover = dict()
+        kwargs_cover['cover_length'] = fix_float(line[9].strip())
+        kwargs_cover['cover_material'] = line[22].strip()
+        kwargs_cover['cover_type'] = line[23].strip()
+        kwargs_cover['cover_weight'] = fix_float(line[24].strip())
+
+        # Gunpowder
+        kwargs_gunpowder = dict()
+        kwargs_gunpowder['gunpowder_type'] = line[25].strip()
+        kwargs_gunpowder['gunpowder_color'] = line[26].strip()
+
+        # Put together
+        for key in ('ammo', 'tip', 'cover', 'calibers', 'projectile', 'gunpowder'):
+            result[key] = locals()['kwargs_' + key]
+
         return result
 
-    def handle(self, *args, **options):
+    def create_ammocover(self, **kwargs):
+        cover, created = AmmoCover.objects.get_or_create(**kwargs)
+        return cover
 
-        # Get the values
+    def create_ammoprojectile(self, **kwargs):
+        projectile, created = AmmoProjectile.objects.get_or_create(**kwargs)
+        return projectile
+
+    def create_ammogunpowder(self, **kwargs):
+        if kwargs['gunpowder_color']:
+            import pdb; pdb.set_trace()
+        gunpowder, created = AmmoGunpowder.objects.get_or_create(**kwargs)
+        return gunpowder
+
+    def create_ammotip(self, **kwargs):
+        tip, created = AmmoTip.objects.get_or_create(**kwargs)
+        return tip
+
+    def handle(self, *args, **options):
         filename = 'ammo-lagoa.csv'
         filepath = os.path.join(os.path.curdir, 'db', filename)
 
         with open(filepath) as f:
             csv_reader = csv.reader(f, delimiter='\t')
 
-            for line in csv_reader:
+            for idx, line in enumerate(csv_reader):
+                if idx == 0:
+                    continue
                 # Save values
-                values = self.get_values(line)
+                result = self.get_values(line)
 
-                print values['ammo_type']
-                # Create ammocover
-
-                # Create the dependencies
+                # Create dependencies
+                values = dict()
+                for key in ('cover', 'projectile', 'gunpowder', 'tip'):
+                    values[key] = getattr(self, 'create_ammo' + key)(**result[key])
